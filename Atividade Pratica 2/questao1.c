d1f765b662601d6be9334adf0796174cde76b510
Gabryel-lima <gabbryellimasi@gmail.com>
2025-07-01 12:27:17 -0300

    Implementa cálculo da área da seção transversal e momento de inércia para vigas
    - Adiciona o arquivo `questao1.c` com a implementação do cálculo da área da seção transversal e do momento de inércia de uma viga com base na largura e altura fornecidas pelo usuário.
    - Inclui macros para facilitar os cálculos e garante a entrada correta de dados.
    - Exibe mensagens informativas sobre os resultados e a aptidão da viga para carga básica.

---
+++ Atividade Pratica 2/questao1.c
@@ -0,0 +1,57 @@
+#include <stdio.h> // Hearder de entrada e saída.
+#include <math.h> // header de funções matemáticas.
+#include <locale.h> // hearder que define um conjunto de convenções culturais usadas para formatação de texto, números, datas, ordenação alfabética, etc.
+
+#define TRANSVERSAL(b, h) (b) * (h) // Macro para o cálculo da área da seção transversal.
+#define INERCIA(b, h) ((b) * (pow(h, 3)) / 12) // Macro para o cálculo do momento de inércia.
+
+int main(void) {
+    /*
+    Declaração de variáveis:
+    - largura: largura da viga.
+    - altura: altura da viga.
+    - transversal: área da seção transversal.
+    - inercia: momento de inércia.
+    */
+
+    printf("\n+++ Questão 1 +++\n");
+
+    int largura, altura; // Variáveis para armazenar a largura e altura da viga.
+    float transversal, inercia; // Variáveis para armazenar a área da seção transversal e o momento de inércia.
+
+    // Salva guarda para o setlocale() para garantir que o programa funcione corretamente em qualquer sistema operacional.
+    if (!setlocale(LC_ALL, "pt_BR.UTF-8")) {
+        fputs("Locale pt_BR.UTF-8 indisponível\n", stderr);
+    }
+
+    // Apenas garantindo que a entrada seja um número inteiro.
+    printf("\nDigite a largura (cm): "); // Solicita a largura da viga.
+    if (scanf("%i", &largura) != 1) {
+        fprintf(stderr, "Erro ao ler a largura.\n");
+        return 1;
+    }
+
+    // Apenas garantindo que a entrada seja um número inteiro.
+    printf("Digite a altura (cm): "); // Solicita a altura da viga.
+    if (scanf("%i", &altura) != 1) {
+        fprintf(stderr, "Erro ao ler a altura.\n");
+        return 1;
+    }
+
+    printf("\n+++ Calculando a área da seção transversal +++"); // Exibe uma mensagem indicando que o programa está calculando a área da seção transversal.
+                
+    transversal = TRANSVERSAL(largura, altura); // Calcula a área da seção transversal.
+    inercia = INERCIA(largura, altura); // Calcula o momento de inércia.
+
+    printf("\n» Área = %.2f cm²", transversal); // Exibe a área da seção transversal.
+    printf("\n» Momento de inercia = %.2f cm⁴", inercia); // Exibe o momento de inércia.
+    
+    if(transversal >= 100) { // Verifica se a área da seção transversal é maior ou igual a 100 cm².
+        printf("\n» A viga está apta para carga básica (A >= 100 cm²).\n\n"); // Exibe uma mensagem indicando que a viga está apta para carga básica.
+    }
+    else { // Se a área da seção transversal não for maior ou igual a 100 cm².
+        printf("\n» A viga não está apta\n\n");
+    }
+
+    return 0;
+}
---
+++ Atividade Pratica 2/questao2.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Hearder de entrada e saída.
+#include <math.h> // header de funções matemáticas.
+#include <locale.h> // hearder que define um conjunto de convenções culturais usadas para formatação de texto, números, datas, ordenação alfabética, etc.
+
+int main(void) {
+
+    return 0;
+}
